37b693eafde4b611f038662740679c1b2c39e9fe
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2024-03-05 12:49:37 +0530

    data types - java & py codes

---
+++ 01_BASICS/01_Basic_Programming_Constructs/02_data_types/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        int x = 5;
+        System.out.println(x); // 5
+
+        float y = 5.0f;
+        System.out.println(y); // 5.0
+
+        double z = 5.0;
+        System.out.println(z); // 5.0
+
+        char c = 'A';
+        System.out.println(c); // A
+
+        String s = "Hello";
+        System.out.println(s); // Hello
+
+        boolean b = true;
+        System.out.println(b); // true
+
+        long l = 1000000000000000000L;
+        System.out.println(l); // 1000000000000000000
+    }
+}
---
+++ 01_BASICS/01_Basic_Programming_Constructs/02_data_types/program.py
@@ -0,0 +1,8 @@
+x = 5
+print(type(x)) # <class 'int'>
+
+y = 5.0
+print(type(y)) # <class 'float'>
+
+z = "Hello"
+print(type(z)) # <class 'str'>
